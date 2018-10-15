b2bf50488a35f1bb9a152bc0fcc4642efcef876c
KedarnathSahu <kedarnath.sahu97@gmail.com>
2018-10-15 13:58:43 +0530

    Initial commit #4

--- app/src/main/java/com/example/cumulations/myapplication/MainActivity.java
+++ app/src/main/java/com/example/cumulations/myapplication/MainActivity.java
@@ -38,7 +38,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         
-
         RequestQueue queue=Volley.newRequestQueue(this);
         queue.add(request);
     }
